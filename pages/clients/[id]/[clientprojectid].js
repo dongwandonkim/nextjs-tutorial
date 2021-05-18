2e33eaae7f184a441d59728edc26b2591d6fe2ba
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-05-17 23:47:57 -0700

    pages & file-based Routing

---
+++ pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SelectedClientProjectPage() {
+  return (
+    <div>
+      <h1>Selected Client Project Page</h1>
+    </div>
+  );
+}
---
+++ pages/portfolio/[id].js
@@ -0,0 +1,7 @@
+import { useRouter } from 'next/router';
+
+export default function PortfolioProjectPage() {
+  const router = useRouter();
+
+  return <div>PortfolioProjectPage</div>;
+}
