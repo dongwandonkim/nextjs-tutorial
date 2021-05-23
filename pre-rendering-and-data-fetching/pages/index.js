375d6745115113ab040a1b475ed281ede173dfb0
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-05-22 20:33:32 -0700

    data-fetching & pre-rendering

---
+++ pre-rendering-and-data-fetching/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <ul>
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+    </ul>
+  );
+}
