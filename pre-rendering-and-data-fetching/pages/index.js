0647dceba396c86219afaee9a08fc8b10360c48a
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-05-24 23:24:24 -0700

    static pre-rendering and data fetching

--- pre-rendering-and-data-fetching/pages/index.js
+++ pre-rendering-and-data-fetching/pages/index.js
@@ -1,13 +1,24 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import fs from 'fs/promises';
+import path from 'path';
 
-export default function Home() {
+export default function HomePage(props) {
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
